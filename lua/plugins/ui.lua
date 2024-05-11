e6ce4c7879f64eb65e579a8318232fd281ee0d49
Mariell Hoversholm <mariell@mardroemmar.dev>
2024-05-12 01:12:00 +0200

    feat: use catppuccin

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,15 +1,25 @@
 return {
+--  {
+--    'sainnhe/gruvbox-material',
+--    lazy = false,
+--    priority = 5000,
+--    config = function()
+--      vim.opt.termguicolors = true
+--      vim.opt.background = 'dark'
+--      vim.g.gruvbox_material_background = 'soft'
+--      vim.g.gruvbox_material_foreground = 'material' -- material, mix, or original; i wanna try the new stuff.
+--      vim.g.gruvbox_material_better_performance = true
+--      vim.cmd [[colorscheme gruvbox-material]]
+--    end,
+--  },
   {
-    'sainnhe/gruvbox-material',
+    'catppuccin/nvim',
     lazy = false,
     priority = 5000,
     config = function()
       vim.opt.termguicolors = true
       vim.opt.background = 'dark'
-      vim.g.gruvbox_material_background = 'soft'
-      vim.g.gruvbox_material_foreground = 'material' -- material, mix, or original; i wanna try the new stuff.
-      vim.g.gruvbox_material_better_performance = true
-      vim.cmd [[colorscheme gruvbox-material]]
+      vim.cmd [[colorscheme catppuccin-macchiato]]
     end,
   },
   {
@@ -34,7 +44,8 @@ return {
     dependencies = { { 'nvim-tree/nvim-web-devicons' } },
     opts = {
       options = {
-        theme = 'gruvbox-material',
+        -- theme = 'gruvbox-material',
+        theme = 'catppuccin-macchiato',
         component_separators = { left = '|', right = '|' },
       },
     },
